f7e127bbfe729803103888d0c432834e7e5c6a04
Lavanya Varshney <149246272+Lavavarshney@users.noreply.github.com>
2025-02-02 19:46:29 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -12,6 +12,7 @@ const userSchema = new mongoose.Schema({
   },
   password: { type: String },
   subscribed: { type: Boolean, default: true }, // Add this field
+  emailsSent: { type: Number, default: 0 }, // Track the number of emails sent
 });
 
 const User = mongoose.model('User', userSchema);
