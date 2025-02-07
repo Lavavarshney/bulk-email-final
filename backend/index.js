6263ea2560e3a8474e3d55afba7da5bcba57dcd7
Lavanya Varshney <149246272+Lavavarshney@users.noreply.github.com>
2025-02-07 23:46:55 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -391,7 +391,7 @@ app.get('/unsubscribe', async (req, res) => {
 // Helper function to send email
 const sendEmailAndNotifyWebhook = async (senderName, recipientEmail, recipientName) => {
   try {
-    const trackingPixelURL = `https://bulk-email-final2.onrender.com/track-open?email=${encodeURIComponent(recipientEmail)}`;
+    const trackingPixelURL = `http://bulk-email-final2.onrender.com/track-open?email=${encodeURIComponent(recipientEmail)}`;
     const personalizedEmailContent = dynamicEmailContent.replace('{{name}}', recipientName);
     const emailContentWithPixel = `${personalizedEmailContent}
       <img src="${trackingPixelURL}" alt="Tracking Pixel" width="1" height="1" style="display: none;" />`;
