71a4086741cb928d82f7cc171eafa7b3bdeaea1c
Lavanya Varshney <149246272+Lavavarshney@users.noreply.github.com>
2025-02-13 21:43:34 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -14,7 +14,7 @@ const userSchema = new mongoose.Schema({
   subscribed: { type: Boolean, default: true }, // Add this field
   emailsSent: { type: Number, default: 0 }, // Track the number of emails sent
   planStatus: {type: String, enum:["free","basic","premium","paid"], default:"free"},
-  emailLimit: {type: Number, default: 10},
+  emailLimit: {type: Number, default: 5},
   emailsOpened: { type: Number, default: 0 }, // Track the number of emails opened
  lastEmailSentAt: { type: Date, default: null }, // Track last email sent timestamp
 lastEmailOpenedAt: { type: Date, default: null }, // Track the last time an email was opened
